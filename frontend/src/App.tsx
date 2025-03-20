b1f78e9ca66b612476fb342fb2be825a4d2b3b15
Augusto Pastrana <aspastrana990@gmail.com>
2025-03-20 06:06:16 -0300

    Add Suspense with fallback

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './i18n';
 
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import useAuth from './hooks/useAuth';
@@ -37,22 +37,37 @@ export default function App() {
     } else {
       setIsLoaded(true);
     }
-  }, []);
+  }, [authenticatedUser]);
 
-  return isLoaded ? (
-    <Router>
-      <Switch>
-        <PrivateRoute exact path="/" component={Dashboard} />
-        <PrivateRoute exact path="/users" component={Users} roles={['admin']} />
-        <PrivateRoute exact path="/profile" component={Profile} />
-        <PrivateRoute exact path="/favorites" component={Favorites} />
-        <PrivateRoute exact path="/courses" component={Courses} />
-        <PrivateRoute exact path="/courses/:id" component={Contents} />
-        <PrivateRoute exact path="/calendar" component={Calendar} />
-        <PrivateRoute exact path="/contact" component={Contact} />
+  return (
+    isLoaded && (
+      <Suspense
+        fallback={
+          <div className="flex h-screen w-screen items-center justify-center">
+            <div className="h-12 w-12 animate-spin rounded-full border-4 border-gray-300 border-t-[#E31D1C]"></div>
+          </div>
+        }
+      >
+        <Router>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute
+              exact
+              path="/users"
+              component={Users}
+              roles={['admin']}
+            />
+            <PrivateRoute exact path="/profile" component={Profile} />
+            <PrivateRoute exact path="/favorites" component={Favorites} />
+            <PrivateRoute exact path="/courses" component={Courses} />
+            <PrivateRoute exact path="/courses/:id" component={Contents} />
+            <PrivateRoute exact path="/calendar" component={Calendar} />
+            <PrivateRoute exact path="/contact" component={Contact} />
 
-        <AuthRoute exact path="/login" component={Login} />
-      </Switch>
-    </Router>
-  ) : null;
+            <AuthRoute exact path="/login" component={Login} />
+          </Switch>
+        </Router>
+      </Suspense>
+    )
+  );
 }
